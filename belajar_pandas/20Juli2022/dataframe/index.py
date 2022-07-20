cd4bc249158af0aa925cb7d36a4e083ebbfb58be
falentinodjoka2801 <falentinodjoka2801@gmail.com>
2022-07-20 18:47:36 +0700

    DataFrame (menjumlahkan kolom, menghapus baris, menghapus kolom)

--- belajar_pandas/20Juli2022/dataframe/index.py
+++ belajar_pandas/20Juli2022/dataframe/index.py
@@ -2,12 +2,33 @@ from numpy import random
 from pandas import DataFrame
 
 try:
+    random.seed(100)
+
     _data           =   random.randn(3, 5)
     _rowIndex       =   ['A', 'B', 'C']
     _columnIndex    =   ['Satu', 'Dua', 'Tiga', 'Empat', 'Lima']
     _dataFrame  =   DataFrame(_data, index=_rowIndex, columns=_columnIndex)
 
+    # print(_dataFrame)
+    # print(type(_dataFrame))
+    # print(_dataFrame['Dua'])
+    # print(type(_dataFrame['Dua']))
+    # print(_dataFrame['Dua']['C'])
+    # print(type(_dataFrame['Dua']['C']))
+
     print(_dataFrame)
-    print(_dataFrame['Dua']['C'])
+
+    _enam               =   _dataFrame['Empat'] + _dataFrame['Lima']
+    _dataFrame['Enam']  =   _enam
+
+    print(_dataFrame)
+
+    _dataFrame  =   _dataFrame.drop('A')
+    
+    print(_dataFrame)
+
+    _dataFrame  =   _dataFrame.drop('Lima', axis=1)
+
+    print(_dataFrame)
 except KeyError as e:
     print(f'Key error => {e}')
