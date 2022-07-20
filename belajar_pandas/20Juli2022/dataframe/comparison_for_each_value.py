e8599414474a819d3df4c4ad7ea0bd4232bfbaa9
Falentino Djoka <falentinodjoka2801@gmail.com>
2022-07-20 22:32:07 +0700

    Filter DataFrame

--- belajar_pandas/20Juli2022/dataframe/comparison_for_each_value.py
+++ belajar_pandas/20Juli2022/dataframe/comparison_for_each_value.py
@@ -3,4 +3,21 @@ from numpy import random
 
 random.seed(100)
 
-_dataFrame  =   random.randn(3, 5)
+_data           =   random.randn(5, 3)
+_rowIndex       =   ['A', 'B', 'C', 'D', 'E']
+_columnIndex    =   ['A', 'B', 'C']
+_dataFrame  =   DataFrame(_data, index=_rowIndex, columns=_columnIndex)
+
+_column_D       =   _dataFrame['A'] + _dataFrame['B'] + _dataFrame['C']
+_dataFrame['D'] =   _column_D
+
+print(_dataFrame)
+
+_dataFrame  =   _dataFrame.drop('C', axis=1)
+print(_dataFrame)
+
+_dataFrame  =   _dataFrame[_dataFrame['D'] >= 1]
+print(_dataFrame)
+
+_dataFrame  =   _dataFrame <= 0
+print(_dataFrame)
